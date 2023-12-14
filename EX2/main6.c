4e2230cf7bd0aae723ea64988a1f926510443ab5
Chenredfish <75733853+Chenredfish@users.noreply.github.com>
2023-12-14 15:25:09 +0800

    Create main6.c

---
+++ EX2/main6.c
@@ -0,0 +1,18 @@
+#include <iostream>
+int main(){
+  int i=1599;
+  if(i<=1500){
+    printf("70元");
+  }
+  else{
+    int n = i-1500;
+    if(n%100){
+      int h = ((n/100)+1)*10;
+      printf("%d元",70+h);
+    }
+    else{
+      printf("%d元", 70+(n/10));
+    }
+  }
+  return 0;
+}
