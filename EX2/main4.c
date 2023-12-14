30140e3b3cc404fe331c89ac3b7a9db2d89f01cc
Chenredfish <75733853+Chenredfish@users.noreply.github.com>
2023-12-14 15:31:27 +0800

    Update main4.c

--- EX2/main4.c
+++ EX2/main4.c
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 int main(){
   int i=1;
   if(i%400 == 0 || (i%4==0 && i%100!=0)){
-    printf("閏年")
+    printf("閏年");
   }
   else{
-    printf("不是閏年")
+    printf("不是閏年");
   }
   return 0;
 }
