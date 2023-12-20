95e30d0bcf68ffd316541c292bf8ff190667ec17
Chenredfish <75733853+Chenredfish@users.noreply.github.com>
2023-12-20 16:31:21 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -3,11 +3,9 @@
 int main()
 {
     int input=7;
-    for(int i1=0; i1<input+1; i1++){
-        for(int i2=1; i2<(input-i1+1); i2++){
-            printf(" ");
-        }
-        for(int i2=0; i2<i1; i2++){
+    for(int i1=1; i1<=input; i1++){
+        printf("%*d ", input-i1+1, i1);
+        for(int i2=1; i2<i1; i2++){
             printf("%d ", i1);
         }
         printf("\n");
